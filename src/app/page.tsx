d2cb622fc4a76549234c223122c4bbc62a7889a2
김재혁 <re2volution@plateer.com>
2025-01-08 13:01:40 +0900

    초기 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="text-Brand-Main-Color font-bold size-10">메인 페이지</div>
-  );
+  return <div className="text-Brand-Main-Color font-bold">메인 페이지</div>;
 }
