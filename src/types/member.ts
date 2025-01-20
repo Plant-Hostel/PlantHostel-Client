5e892cfd8d5ee7d5c5937d8b638ec6baff4ae0d1
김재혁 <re2volution@plateer.com>
2025-01-20 13:12:03 +0900

    member type 변경

--- src/types/member.ts
+++ src/types/member.ts
@@ -2,15 +2,10 @@ export interface Member {
   userId: string;
   password: string;
   passwordCheck: string;
-  name: string;
   nickname: string;
   hp: string;
   email: string;
   zipcode: string;
   address: string;
   addressDetail: string;
-  residenceType: string;
-  gender: string;
-  favoritePlant: string;
-  intro: string;
 }
